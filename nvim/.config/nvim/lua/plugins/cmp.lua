141421f1b62c52d0a670405285010417d132ea49
MoritzSchwerer <moritz.schwerer@gmx.de>
2023-02-07 19:21:38 +0100

    temp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -32,13 +32,14 @@ return {
                     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-f>'] = cmp.mapping(function(fallback)
                         if require('luasnip').jumpable(1) then
                             require('luasnip').jump(1)
                         else
                             fallback()
-                    end)
+                        end
+                    end),
                 }),
                 sources = {
                     { name = 'copilot' },
@@ -50,4 +51,7 @@ return {
             }
         end,
     },
+    {
+
+    }
 }
