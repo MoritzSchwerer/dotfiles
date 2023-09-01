7e7ca8b49cfa30a6075dba7a105b6357a496a401
MoritzSchwerer <moritz.schwerer@gmx.de>
2023-09-01 13:45:23 +0200

    added nvim mappings

--- nvim/.config/nvim/lua/config/mappings.lua
+++ nvim/.config/nvim/lua/config/mappings.lua
@@ -13,3 +13,14 @@ keymap('n', '<leader>af', "<cmd>Telescope find_files<cr>", opts)
 keymap('n', '<leader>ag', "<cmd>Telescope live_grep<cr>", opts)
 keymap('n', '<leader>ab', "<cmd>Telescope buffers<cr>", opts)
 keymap('n', '<leader>ah', "<cmd>Telescope help_tags<cr>", opts)
+
+-- usefull stuff
+keymap('n', "<leader>p", "\"_dp", opts)
+keymap('v', ">", ">gv", opts)
+keymap('v', "<", "<gv", opts)
+
+-- greatest remaps ever
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
--- nvim/.config/nvim/lua/plugins/edit.lua
+++ nvim/.config/nvim/lua/plugins/edit.lua
@@ -1,26 +1,26 @@
 return {
-	{
-		'windwp/nvim-autopairs',
-	    event = "InsertEnter",
-		config = true,
-	},
-	{
-		'numToStr/Comment.nvim',
-		event = "VeryLazy",
-		opts = {
-			toggler = {
-				line = 'co',
-			},
-			opleader = {
-				line = 'co',
-			},
-		},
-		config = true,
-	},
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true,
+    },
+    {
+        'numToStr/Comment.nvim',
+        event = "VeryLazy",
+        opts = {
+            toggler = {
+                line = 'co',
+            },
+            opleader = {
+                line = 'co',
+            },
+        },
+        config = true,
+    },
     {
         'mbbill/undotree',
         keys = {
-            { "<leader>u", "<cmd>UndotreeToggle<cr>", desc="Toggle Undutree" },
+            { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" },
         },
     },
 }
