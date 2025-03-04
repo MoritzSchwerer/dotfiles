8702d4625b01bdd2438513b1d4dc8d233d4dedf3
MoritzSchwerer <moritz.schwerer@gmx.de>
2025-03-04 08:27:06 +0100

    added lock_icon.png for lockscreen into config

--- i3/.local/bin/i3lock.sh
+++ i3/.local/bin/i3lock.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-icon="$HOME/Downloads/icon.png"
+icon="$HOME/.config/i3/lock_icon.png"
 tmpbg='/tmp/screen.png'
 
-#(( $# )) && { icon=$1; }
+(( $# )) && { icon=$1; }
 
 scrot "$tmpbg" -o
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
