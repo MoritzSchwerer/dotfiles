56e16bdc6842e57f450fb971e5c5e71890ebfaa8
MoritzSchwerer <moritz.schwerer@gmx.de>
2025-03-30 14:39:25 +0200

    added logging macro to nvim config for python and cpp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -19,3 +19,23 @@ require('lazy').setup('plugins', {
     defaults = { lazy = true }
 })
 require('config')
+
+-----------------------------------
+-------------- Macros -------------
+
+-- @l will print out the value of the highlighted variable
+vim.api.nvim_create_augroup("LoggingMacro", { clear = true })
+local log_commands = {
+    python = "yoprint('pa €kb: ', pa)",
+    cpp = "yostd::cout << 'pa €kb:' << pa << std::endl;",
+}
+vim.api.nvim_create_autocmd("FileType", {
+    group = "LoggingMacro",
+    pattern = vim.tbl_keys(log_commands),
+    callback = function(args)
+        local command = log_commands[args.match]
+        if command then
+            vim.fn.setreg("l", command)
+        end
+    end
+})
